a6b490f96b610ef3f099f3e2b6ab16de88356ae6
flavia-dantas <flavia.dantas2@hotmail.com>
2022-09-04 21:29:20 -0300

    feat: reset css, breakpoints and variables

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css";
+import "./index.css";
+import Menu from "./pages/Menu";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <div>Aluroni</div>
-  </React.StrictMode>
+    <Menu />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
